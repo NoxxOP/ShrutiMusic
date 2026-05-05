208c3cf3fdd88341f1df18c9ace920c31723ec30
sikander025 <sikander025.tg@gmail.com>
2026-05-06 00:32:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask
 import threading
 import os
+import subprocess
 import time
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot Running 🚀"
+    return "Astarr Music Bot Running 🚀"
 
 def run_bot():
     while True:
         print("Starting bot...")
-        os.system("python3 -m ShrutiMusic")
-        print("Bot stopped. Restarting in 30 sec...")
-        time.sleep(30)
+        process = subprocess.Popen(["python3", "-m", "ShrutiMusic"])
+        process.wait()  # wait until bot stops
+        print("Bot stopped. Restarting in 10 sec...")
+        time.sleep(10)
 
 if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
+    t = threading.Thread(target=run_bot)
+    t.daemon = True
+    t.start()
+
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
